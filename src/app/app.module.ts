8a5ab9f35eacd999d8c0640117d1cb86e954c26b
F-V <32668262+F-V@users.noreply.github.com>
2018-03-19 15:05:50 +0100

    Config and use navigator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { LaunchNavigator } from '@ionic-native/launch-navigator';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -28,6 +30,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    LaunchNavigator,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-navigator';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  destination:string;
+  start:string;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+      public navCtrl: NavController,
+      private launchNavigator: LaunchNavigator,
+      private toastCtrl:ToastController
+  ) {
+    this.start = "";
+    this.destination = "50.8245564,3.2496834";
   }
 
+  navigate(){
+    let options: LaunchNavigatorOptions = {
+      start: this.start
+    };
+
+    this.launchNavigator.navigate(this.destination, options)
+        .then(
+            success => this.presentToast('Launched navigator'),
+            error => this.presentToast('Error launching navigator: ' + error)
+    );
+  }
+
+  presentToast(message:string) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000,
+      position: 'top'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
+  }
 }
